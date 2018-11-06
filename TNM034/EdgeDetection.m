9004c891f417fc2494d4db696533732df4383f54
isabellerosenquist <isabelle.rosenquist@gmail.com>
2018-11-06 14:52:56 +0100

    Added files and images

---
+++ TNM034/EdgeDetection.m
@@ -0,0 +1,15 @@
+function Image_return = EdgeDetection(Im)
+%UNTITLED5 Summary of this function goes here
+%   Detailed explanation goes here
+
+Im_grey =rgb2gray(Im);
+%imshow(Im_grey);
+imbinarize
+%Image_return= edge(Im_grey, 'approxcanny');
+
+
+
+
+
+end
+
---
+++ TNM034/tnm034.m
@@ -0,0 +1,52 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%
+function strout = tnm034(Im)
+%
+% Im: Input image of captured sheet music. Im should be in
+% double format, normalized to the interval [0,1]
+%
+% strout: The resulting character string of the detected notes.
+% The string must follow the pre-defined format, explained below.
+%
+% Your program code…
+
+%% Preprocess
+
+%Detect papper 
+Im = EdgeDetection(Im);
+
+%Reduce blur
+
+
+%Reduce rotation
+
+
+%Reduce perspective
+
+
+%Reduce noise
+
+
+
+%% Detect
+
+%Detect G-klak
+
+
+
+
+
+%Detect staff lines
+
+%Detect notes
+
+%% Convert
+
+
+
+
+strout = Im;
+
+
+
+imshow(Im);
+%%%%%%%%%%%%%%%%%%%%%%%%%%
