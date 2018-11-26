47cec9f0f46040358dd3660084262f8ac62373a0
Daniel <daniel.olsson1992@gmail.com>
2018-11-26 11:28:14 +0100

    SortNoteHeads

--- TNM034/SortNoteHeads.m
+++ TNM034/SortNoteHeads.m
@@ -1,12 +1,32 @@
 function [Position] = SortNoteHeads(NoteHeadsPos,GKlausPos, StaffLinesPos, Length)
     Min = min(min(StaffLinesPos));   
     Top = Min-Length*5;
-    t = zeros(size(GKlausPos));
-    t = GKlausPos(:,1).Centroid(1,2);
+
+    %Get a vector from struct
+    vect = cell2mat( struct2cell(GKlausPos(:,1)));
+    %Make the vector to a matrix
+    s = size(vect, 2);
+    matXY = reshape (vect, [2,s/2])';
     
-    A = BW>0.5;
+    %Make a mask 
+    mask = matXY(:,2)>Top;
+    
+    %Avarage GKlaus position
+    GklausMask = (matXY.*mask);
+    AvarageGKlaus = sum(GklausMask(:,1))/sum(mask);    
     
     
-    Position = 0;
+    %Get a vector from struct
+    vect = cell2mat( struct2cell(NoteHeadsPos(:,1)));
+    s = size(vect, 2);
+    %Make the vector to a matrix
+    matXY = reshape (vect, [2,s/2])';
+    
+    % Mask for notehead
+    NoteHeadMask = matXY(:,1) > (AvarageGKlaus+10);
+    
+    % Noteheads
+    NoteHeads = (matXY.*NoteHeadMask);
+    Position = NoteHeads;
 end
 
